e89316c8b795a6aa83d68fd2daf8c5fa9958e637
Pedro-Black <phrod2007@gmail.com>
2023-03-13 11:51:00 -0300

    alteração

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# black
+# super
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,60 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+    const nome = 'João'
+    const idade = 25
 
-const contador = ref(0)
-
-function incrementarContador() {
-  contador.value++
-}
-function decrementarContador() {
-  contador.value--
-}
-
-onMounted(() => {
-  console.log(`O valor inicial do contador é ${contador.value}.`)
-})
-</script>
-
-<script>
-export default {
-  data() {
-    return {
-      contador: 0
+    function inverter(texto) {
+        return texto.split('').reverse().join('')
     }
-  },
-  methods: {
-    incrementarContador() {
-      this.contador++
-    },
-    decrementarContador() {
-      this.contador--
-    }
-  },
-  mounted() {
-    console.log(`O valor inicial do contador é ${this.contador}.`)
-  }
-}
 
+    function inverterMaiusculo(texto) {
+        const invertido = inverter(texto)
+        return invertido.toUpperCase()
+    }
+
+    function saudacao(){
+      return `Olá, ${nome}!`
+    }
 </script>
 
 <template>
-<button @click="incrementarContador">Incrementar</button>
-  <button @click="decrementarContador">Decrementar</button>
-  <p>Valor do contador é: {{ contador }}</p>
+    <div class="info">
+        <h1>Exemplo 1 - Mostrando informações na tela</h1>
+        <p>Nome: {{ nome }} </p>
+        <p>Idade: {{ idade }}  anos</p>
+        <hr />
+        <p>Para completar 50 anos faltam: {{ 50 - idade }}  anos</p>
+        <p>O nome tem {{ nome.length }}  caracteres</p>
+        <p>O nome invertido em maiúsculas é: {{ inverterMaiusculo(nome) }} </p>
+        <hr />Exemplo 1 - Mostrando informações na tela
+Nome: João
+
+Idade: 25 anos
+
+Para completar 50 anos faltam: 25 anos
+
+O nome tem 4 caracteres
+
+O nome invertido em maiúsculas é: OÃOJ
+
+Exemplo de saudação usando função
+
+Olá, João!
+        <p> Exemplo de saudação usando função</p>
+        <p> {{ saudacao() }}  </p>
+    </div>
 </template>
 
 <style scoped>
-button {
+.info {
+  background-color: rgba(35,12,22,1);
+  padding: 20px 30px;
+  color: rgb(183, 210, 219);
+  border-radius: 10px;
+}
+
+h1 {
+  color: white;
   font-weight: bold;
+  font-size: 1.5em;
 }
 </style>
